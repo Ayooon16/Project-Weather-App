7b35296fe73da2eae21ca9b3ccfe661f8856590f
ayooon <michaunapierala@gmail.com>
2024-04-07 18:05:18 +0200

    Add styling

--- src/modules/createCard.js
+++ src/modules/createCard.js
@@ -4,28 +4,47 @@ import getUnitTitle from "./getUnitTitle";
 export default async function createCard(name, american) {
   try {
     const weather = await fetchData(name, american);
-    console.log(weather);
-    const card = document.getElementById("card");
+    const location = weather.details
+    const date = new Date(location.localtime_epoch*1000)
+    const card = document.getElementById("card")
+    const details = document.createElement('div')
+details.className='details'
+    card.innerHTML = "";
+    const cardHeader=document.createElement('div')
+    cardHeader.className='cardHeader'
+    const locationName=document.createElement('p')
+    locationName.className='cardName'
+    locationName.innerText=`${location.name}, ${location.country}`
+    const locationDetails = document.createElement('p')
+    locationDetails.className='cardDetails'
+    locationDetails.innerText=`${date.toLocaleString('default', { weekday: 'long' })} ${date.getDate()} ${date.toLocaleString('default', { month: 'long' })} ${date.getFullYear()} | ${date.getHours()}:${date.getMinutes()}`
+    cardHeader.appendChild(locationName)
+    cardHeader.appendChild(locationDetails)
+    card.appendChild(cardHeader)
     const img = document.createElement("img");
     img.src = weather.icon;
-    card.appendChild(img);
+    details.appendChild(img);
+    const data = document.createElement('div')
+    data.className='data'
     Object.entries(weather).forEach(([key, value]) => {
-        if (key!=='icon'){
-            const titleUnit =getUnitTitle(american, key)
-            const title=titleUnit[0]
-            const unit=titleUnit[1]
-            const position = document.createElement('div')
-            position.className=key
-            const positionTitle = document.createElement('p')
-            positionTitle.innerText=title
-            const positionData = document.createElement('p')
-            positionData.innerText=value+unit
-            position.appendChild(positionTitle)
-            position.appendChild(positionData)
-            card.appendChild(position)
-        }
-    });
-
+      if (key !== "icon"&& key!=='details') {
+        const titleUnit = getUnitTitle(american, key);
+        const title = titleUnit[0];
+        const unit = titleUnit[1];
+        const position = document.createElement("div");
+        position.className = "position";
+        const positionTitle = document.createElement("p");
+        positionTitle.innerText = title;
+        const positionData = document.createElement("p");
+        positionData.innerText = value + unit;
+        position.appendChild(positionTitle);
+        position.appendChild(positionData);
+        data.appendChild(position);
+      }
+    })
+    details.appendChild(data)
+    card.appendChild(details)
+    ;
   } catch (error) {
     console.error("Error creating card:", error);
   }
--- src/modules/getUnitTitle.js
+++ src/modules/getUnitTitle.js
@@ -1,46 +1,46 @@
 export default function getUnitTitle(american, value) {
-let unit
-let title
-    switch (value) {
-    case 'temperature':
-        unit = american ? "°F" : "°C"
-        title=value
-        break;
-    case 'feelsLike':
-        unit = american ? "°F" : "°C"
-        title='feels like'
-        break;
-    case 'cloudiness':
-        unit = '%'
-        title=value
-        break;
-    case 'humidity':
-        unit = '%'
-        title=value
-        break;
-    case 'precipation':
-        unit = american ? "in" : "mm"
-        title=value
-        break;
-    case 'windSpeed':
-        unit = american ? "mph" : "kph"
-        title='wind speed'
-        break;
-    case 'pressure':
-        unit = american ? "in" : "mb"
-        title=value
-        break;
-    case 'visibility':
-        unit = american ? "miles" : "miles"
-        title=value
-        break;
-    case 'uv':
-        unit = ''
-        title=value
-        break;
+  let unit;
+  let title;
+  switch (value) {
+    case "temperature":
+      unit = american ? "°F" : "°C";
+      title = value;
+      break;
+    case "feelsLike":
+      unit = american ? "°F" : "°C";
+      title = "feels like";
+      break;
+    case "cloudiness":
+      unit = "%";
+      title = value;
+      break;
+    case "humidity":
+      unit = "%";
+      title = value;
+      break;
+    case "precipation":
+      unit = american ? "in" : "mm";
+      title = value;
+      break;
+    case "windSpeed":
+      unit = american ? "mph" : "kph";
+      title = "wind speed";
+      break;
+    case "pressure":
+      unit = american ? "in" : "mb";
+      title = value;
+      break;
+    case "visibility":
+      unit = american ? "miles" : "km";
+      title = value;
+      break;
+    case "uv":
+      unit = "";
+      title = value;
+      break;
 
     default:
-        break;
-}
-  return [title,unit]
+      break;
+  }
+  return [title, unit];
 }
