ed35ddacfc1a01bc3816e9a8975dfb831673cbec
ayooon <michaunapierala@gmail.com>
2024-04-07 15:51:02 +0200

    Create modules

---
+++ src/modules/createCard.js
@@ -0,0 +1,3 @@
+export default function createCard(data, american){
+    
+}
---
+++ src/modules/fetchData.js
@@ -0,0 +1,26 @@
+export default function fetchData(city, american) {
+    fetch(
+      `https://api.weatherapi.com/v1/current.json?key=5589a5b1b0f545c1a04164843240304&q=${city}`,
+    )
+    .then(response => {
+      if (!response.ok) {
+          throw new Error('Failed to fetch weather data');
+      }
+      return response.json();
+  })
+      .then((weather) => {
+        const weatherData = weather.current;
+        const wth = {
+          temperature: american ? weatherData.temp_f : weatherData.temp_c,
+          feelsLike: american ? weatherData.feelslike_f : weatherData.feelslike_c,
+          humidity: weatherData.humidity,
+          icon: weatherData.condition.icon,
+          windSpeed: american ? weatherData.wind_mph : weatherData.wind_kph,
+          pressure: american ? weatherData.pressure_in : weatherData.pressure_mb,
+          visibility: american ? weatherData.vis_miles : weatherData.vis_km,
+          cloudiness: weatherData.cloud,
+        };
+        console.log(wth, american);
+      })
+      
+  }
