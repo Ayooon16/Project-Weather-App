33afe96cccee5c97f6c36e13d7835655182cde18
ayooon <michaunapierala@gmail.com>
2024-04-07 15:42:11 +0200

    Add handling errors

--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
 import "./style.css";
 
-function fetchData(city, american){
-    fetch('https://api.weatherapi.com/v1/current.json?key=5589a5b1b0f545c1a04164843240304&q=katowic').then((response) => response.json()).then((weather) =>{
-    const weatherData = weather.current 
-    const wth = { 
-     temperature : ((american)? weatherData.temp_f : weatherData.temp_c),  
-     feelsLike : ((american)? weatherData.feelslike_f : weatherData.feelslike_c),  
-     humidity : weatherData.humidity,
-     icon : weatherData.condition.icon,
-     windSpeed : ((american)? weatherData.wind_mph : weatherData.wind_kph),
-     pressure: ((american)? weatherData.pressure_in : weatherData.pressure_mb),
-     visibility :((american)? weatherData.vis_miles : weatherData.vis_km),
-     cloudiness : weatherData.cloud
+function fetchData(city, american) {
+  fetch(
+    `https://api.weatherapi.com/v1/current.json?key=5589a5b1b0f545c1a04164843240304&q=${city}`,
+  )
+  .then(response => {
+    if (!response.ok) {
+        throw new Error('Failed to fetch weather data');
     }
- console.log(wth, american)   
+    return response.json();
 })
+    .then((weather) => {
+      const weatherData = weather.current;
+      const wth = {
+        temperature: american ? weatherData.temp_f : weatherData.temp_c,
+        feelsLike: american ? weatherData.feelslike_f : weatherData.feelslike_c,
+        humidity: weatherData.humidity,
+        icon: weatherData.condition.icon,
+        windSpeed: american ? weatherData.wind_mph : weatherData.wind_kph,
+        pressure: american ? weatherData.pressure_in : weatherData.pressure_mb,
+        visibility: american ? weatherData.vis_miles : weatherData.vis_km,
+        cloudiness: weatherData.cloud,
+      };
+      console.log(wth, american);
+    })
+    
 }
-
-fetchData('asd', false)
+fetchData("a", false);
